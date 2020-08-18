4ca13b4b313c1965967e7d99c2d18ef2e1539ce6
Cristian Rodriguez <cfrodca@gmail.com>
2020-08-18 11:22:12 -0500

    Update build.bat
    Wireshark 3.3 support

--- build.bat
+++ build.bat
@@ -1,12 +1,11 @@
-@if "%vcinstalldir%"=="" call "c:\program files (x86)\microsoft visual studio 14.0\vc\bin\x86_amd64\vcvarsx86_amd64.bat"
+@if "%vcinstalldir%"=="" call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 @setlocal
-@set wireshark_source_dir=..\wireshark-2.6.0
-@set wireshark_build_dir=..\wireshark-2.6.0-build64
+@set wireshark_source_dir=..\wireshark
+@set wireshark_build_dir=..\wsbuild64
 @set wireshark_run_dir=%wireshark_build_dir%\run\RelWithDebInfo
-@set gtk2_dir=..\wireshark-win64-libs-2.6\gtk2
-@set libgcrypt_dir=..\wireshark-win64-libs-2.6\libgcrypt-1.7.6-win64ws
+@set gtk2_dir=..\wireshark-win64-libs\vcpkg-export-20190318-win64ws\installed\x64-windows
+@set libgcrypt_dir=..\wireshark-win64-libs\libgcrypt-1.8.3-win64ws
 
-cl.exe /nologo /O2 /I%wireshark_source_dir% /I%wireshark_build_dir% /I%gtk2_dir%\include\glib-2.0 /I%gtk2_dir%\lib\glib-2.0\include /I%libgcrypt_dir%\include /LD dlms.c %wireshark_run_dir%\wireshark.lib %gtk2_dir%\lib\glib-2.0.lib %libgcrypt_dir%\bin\libgcrypt-20.lib
-
-copy dlms.dll %wireshark_run_dir%\plugins\2.6\epan\dlms.dll
+cl.exe /nologo /O2 /I%wireshark_source_dir% /I%wireshark_build_dir% /I%gtk2_dir%\include /I%gtk2_dir%\include\glib /I%libgcrypt_dir%\include /LD dlms.c %wireshark_run_dir%\wireshark.lib %gtk2_dir%\lib\glib-2.0.lib %libgcrypt_dir%\bin\libgcrypt-20.lib
+copy dlms.dll %wireshark_run_dir%\plugins\3.3\epan\dlms.dll
